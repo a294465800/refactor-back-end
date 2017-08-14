f1260b73d42f2682b86bce9d25062c993c9302c1
SHINING <jack7770126@126.com>
2017-08-14 18:21:40 +0800

    [new function] finish the app change button.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,30 @@
 /**
  * Created by Administrator on 2017/8/14.
  */
+
+/**
+ * 切换APP对象
+ * */
+new Vue({
+	el: '#top_app',
+	data: {
+		apps: [{
+			id: 1,
+			name: '潮南'
+		},{
+			id: 2,
+			name: '潮汕'
+		},{
+			id: 3,
+			name: '潮阳'
+		}],
+		current: 0
+	},
+	methods: {
+
+		//修改当前APP
+		changeApp: function (command) {
+			this.current = command
+		}
+	}
+})
